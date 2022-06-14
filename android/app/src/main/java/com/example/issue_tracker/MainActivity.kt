1d16e64e9abc4b79f19293ee691ca6d51796a890
hwj <jminie@naver.com>
2022-06-14 10:36:55 +0900

    refactor: import 정리 및 xml 코드 정리

--- android/app/src/main/java/com/example/issue_tracker/MainActivity.kt
+++ android/app/src/main/java/com/example/issue_tracker/MainActivity.kt
@@ -5,10 +5,6 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
