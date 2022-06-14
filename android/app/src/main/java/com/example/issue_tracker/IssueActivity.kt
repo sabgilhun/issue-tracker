b08b63beb34e520deaea49df2729dd02ada68ac5
junseokseo <junseoks9306@gmail.com>
2022-06-14 17:00:51 +0900

    feat: issue 액티비티 생성 및 바텀 네비게이션 추가

---
+++ android/app/src/main/java/com/example/issue_tracker/IssueActivity.kt
@@ -0,0 +1,11 @@
+package com.example.issue_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IssueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_issue)
+    }
+}
