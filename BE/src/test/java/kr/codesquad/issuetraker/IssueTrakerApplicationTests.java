e7ffe504796747794baf0e14671f1deaaf20faee
Senglee Choi <tmdfl1027@gmail.com>
2022-06-15 16:06:00 +0900

    Chore: 백엔드 프로젝트 초기 설정

---
+++ BE/src/test/java/kr/codesquad/issuetraker/IssueTrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad.issuetraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
