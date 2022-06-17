2ab7a395f1950662b9d038c425c521605188f3ee
Senglee Choi <tmdfl1027@gmail.com>
2022-06-17 16:36:53 +0900

    Feat: 데이터베이스 연결
    로컬 작업에 필요한 H2 데이터베이스 연결 및 설정.

---
+++ BE/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user (display_name, email, password, profile_image_url) values ('lee', 'lee@lee.com', 'pwpw', 'image.jpg');
+insert into label (name, background_color, letter_color, description) values ('feature', '0xFFFFFF', '0x000000', 'test label');
+insert into milestone (title, is_deleted) values ('test milestone', false);
