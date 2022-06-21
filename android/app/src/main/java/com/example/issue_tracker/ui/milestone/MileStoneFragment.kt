b0fdc22069aa7cc12149fbedff2f0d88eb544b5b
junseokseo <junseoks9306@gmail.com>
2022-06-21 19:12:06 +0900

    feat: 저장한 마일스톤 보여줄 프래그먼트 생성 및 리사이클러 뷰 지정

--- android/app/src/main/java/com/example/issue_tracker/ui/milestone/MileStoneFragment.kt
+++ android/app/src/main/java/com/example/issue_tracker/ui/milestone/MileStoneFragment.kt
@@ -4,18 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.issue_tracker.R
+import com.example.issue_tracker.common.repeatOnLifecycleExtension
+import com.example.issue_tracker.databinding.FragmentMileStoneBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class MileStoneFragment : Fragment() {
 
+    private lateinit var binding: FragmentMileStoneBinding
+    private val viewModel: MileStoneViewModel by viewModels()
+    private val adapter = MileStoneAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mile_stone, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mile_stone, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvMileStone.adapter = adapter
+
+        val findNavController = findNavController()
+        setUpAddMileStoneButton(findNavController)
+        observeMileStoneList()
+    }
+
+    private fun setUpAddMileStoneButton(navController: NavController) {
+        binding.ibAddNewMileStone.setOnClickListener {
+            navController.navigate(R.id.action_mileStoneFragment_to_mileStoneAddFragment)
+        }
+    }
+
+    private fun observeMileStoneList() {
+        viewLifecycleOwner.repeatOnLifecycleExtension(Lifecycle.State.STARTED) {
+            viewModel.mileStoneList.collect { mileStoneList ->
+                adapter.submitList(mileStoneList)
+            }
+        }
+    }
 }
