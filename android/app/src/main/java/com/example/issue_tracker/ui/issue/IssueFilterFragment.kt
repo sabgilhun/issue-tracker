7c0f092c80cce1d70739f750fe5a6b852eb96133
hwj <jminie@naver.com>
2022-06-21 16:35:58 +0900

    feat: 상태 팝업 메뉴 구현

--- android/app/src/main/java/com/example/issue_tracker/ui/issue/IssueFilterFragment.kt
+++ android/app/src/main/java/com/example/issue_tracker/ui/issue/IssueFilterFragment.kt
@@ -3,8 +3,10 @@ package com.example.issue_tracker.ui.issue
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -29,6 +31,12 @@ class IssueFilterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val findNavController = findNavController()
         goBackIssue(findNavController)
+
+        binding.ibFilterButtonStatus.setOnClickListener {
+            val statusPopupMenu = PopupMenu(requireContext(), it)
+            statusPopupMenu.menuInflater.inflate(R.menu.issue_filter_status_menu, statusPopupMenu.menu)
+            statusPopupMenu.show()
+        }
     }
 
     private fun goBackIssue(findNavController: NavController) {
