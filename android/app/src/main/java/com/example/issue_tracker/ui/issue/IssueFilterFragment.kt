87141c9e911b7f351fc9d81048b94cf69110dd34
hwj <jminie@naver.com>
2022-06-21 12:13:09 +0900

    feat: 필터 UI 구현

---
+++ android/app/src/main/java/com/example/issue_tracker/ui/issue/IssueFilterFragment.kt
@@ -0,0 +1,25 @@
+package com.example.issue_tracker.ui.issue
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.issue_tracker.R
+import com.example.issue_tracker.databinding.FragmentIssueFilterBinding
+
+
+class IssueFilterFragment : Fragment() {
+
+    lateinit var binding: FragmentIssueFilterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_issue, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+}
