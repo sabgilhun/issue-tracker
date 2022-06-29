1b583a92206ed0fa7ab823b9f04034e5cdeecad1
junseokseo <junseoks9306@gmail.com>
2022-06-29 12:21:32 +0900

    feat: issue detail 화면 구현

---
+++ android/app/src/main/java/com/example/issue_tracker/ui/issue/IssueDetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.issue_tracker.ui.issue
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class IssueDetailViewModel @Inject constructor() : ViewModel() {
+
+
+
+}
