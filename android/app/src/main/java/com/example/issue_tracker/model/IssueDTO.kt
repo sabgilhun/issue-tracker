389921b7797a5bd33f7ab5a20030e8f0eccf38f6
junseokseo <junseoks9306@gmail.com>
2022-06-30 16:23:55 +0900

    fix: labelDTO 내부 클래스로 labelDTOItem클래스 추가 및 Issue 클래스 변환 Companion Object 함수 생성

--- android/app/src/main/java/com/example/issue_tracker/model/IssueDTO.kt
+++ android/app/src/main/java/com/example/issue_tracker/model/IssueDTO.kt
@@ -1,78 +1,37 @@
 package com.example.issue_tracker.model
 
-import com.example.issue_tracker.model.LabelDTO.Companion.toLabel
 import com.google.gson.annotations.SerializedName
 
 data class IssueDTO(
     @SerializedName("issues")
     val issues: List<IssueDTOItem>,
-)
-
-data class LabelListDTO(
-    @SerializedName("labels")
-    val labels: List<LabelDTO>,
-)
-
-data class IssueDTOItem(
-    @SerializedName("description")
-    val description: String,
-    @SerializedName("issueId")
-    val issueId: Int,
-    @SerializedName("label")
-    val label: LabelDTO,
-    @SerializedName("milestoneTitle")
-    val milestoneTitle: String,
-    @SerializedName("title")
-    val title: String,
-)
+) {
+    data class IssueDTOItem(
+        @SerializedName("description")
+        val description: String,
+        @SerializedName("issueId")
+        val issueId: Int,
+        @SerializedName("label")
+        val label: LabelDTO.LabelDTOItem,
+        @SerializedName("milestoneTitle")
+        val milestoneTitle: String,
+        @SerializedName("title")
+        val title: String,
+    )
+}
 
 data class LabelDTO(
-    @SerializedName("backgroundColor")
-    val backgroundColor: String,
-    @SerializedName("description")
-    val description: String,
-    @SerializedName("id")
-    val id: Int,
-    @SerializedName("name")
-    val name: String,
+    @SerializedName("labels")
+    val labels: List<LabelDTOItem>,
 ) {
-    companion object {
-        fun LabelDTO.toLabel(): Label {
-            val parsedColorString = this.backgroundColor.substring(2)
-            val color = buildString {
-                append("#")
-                append("FF")
-                append(parsedColorString)
-            }
-
-            return Label(
-                labelId = requireNotNull(this.id),
-                labelTitle = requireNotNull(this.name),
-                labelColor = color,
-                labelContents = this.description
-            )
-        }
-    }
+    data class LabelDTOItem(
+        @SerializedName("backgroundColor")
+        val backgroundColor: String,
+        @SerializedName("description")
+        val description: String,
+        @SerializedName("id")
+        val id: Int,
+        @SerializedName("name")
+        val name: String,
+    )
 }
-
-fun List<IssueDTOItem>.toClientIssue(): MutableList<Issue> {
-    return this.map { issueDTOItem ->
-        val issueId = requireNotNull(issueDTOItem.issueId)
-        val mileStone = issueDTOItem.milestoneTitle
-        val title = requireNotNull(issueDTOItem.title)
-        val contents = issueDTOItem.description
-        val label = issueDTOItem.label.toLabel()
-        Issue(
-            issueId = issueId,
-            mileStone = mileStone,
-            title = title,
-            contents = contents,
-            label = label
-        )
-    }.toMutableList()
-}
-
-fun List<LabelDTO>.toLabelList(): MutableList<Label> =
-    this.map { labelDTO ->
-        labelDTO.toLabel()
-    }.toMutableList()
